b8d68dc3aafd8ec105ad294e640f1a8e5c92480a
sajid-ahmed2816 <sajid.ahmed2816@gmail.com>
2023-07-27 21:53:28 +0500

    third commit

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,6 +2,7 @@ import Header from "../components/Header"
 import Footer from "../components/Footer"
 import { useSelector, useDispatch } from "react-redux";
 import { remove } from "../config/redux/reducer/cartSlice";
+import '../App.css';
 
 function Cart() {
   const cartData = useSelector(a => a.Cart);
@@ -14,13 +15,61 @@ function Cart() {
     <>
       <Header />
 
-      {cartData.map((x, i) => (
-        <div key={i}>
-          <img src={x.image} width="100px" />
-          <h2>{x.title}</h2>
-          <button onClick={() => handleRemove(x.id)}>Remove From Cart</button>
+      {/* Cart items table */}
+      <section className="cart-table">
+        <div className="container">
+          <table>
+            <thead>
+              <tr>
+                <th>
+                  <td>x</td>
+                </th>
+                <th>
+                  <td>
+                    Products    
+                  </td>
+                </th>
+                <th>
+                  <td>
+                    Price
+                  </td>
+                </th>
+                <th>
+                  <td>
+                    Quantity
+                  </td>
+                </th>
+                <th>
+                  <td>
+                    Subtotal
+                  </td>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {cartData.map((x, i) => (
+                <tr key={i}>
+                  <td>
+                    <button onClick={() => handleRemove(x.id)}>x</button>
+                  </td>
+                  <td>
+                    <img src={x.image} width="100px" />
+                  </td>
+                  <td>
+                    <p>{x.price}</p>
+                  </td>  
+                  <td>
+                    <p>1</p>
+                  </td>  
+                  <td>
+                    <p>{x.price}</p>
+                  </td>  
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-      ))}
+      </section>
 
       <Footer />
     </>
