d43463fe798ac2bd8b72596738a088efba1beb50
sajid-ahmed2816 <sajid.ahmed2816@gmail.com>
2023-08-15 12:02:23 +0500

    store page

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -4,6 +4,16 @@ function Store() {
   return (
     <>
       <Header />
+
+      <section className="stores">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6"></div>
+            <div className="col-md-6"></div>
+          </div>
+        </div>
+      </section>
+
       <Footer />
     </>
   )
