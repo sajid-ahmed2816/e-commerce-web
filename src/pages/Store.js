6faf8a7f321a7c9db5547b8edaf57348ab5adbbf
sajid-ahmed2816 <sajid.ahmed2816@gmail.com>
2023-08-17 05:51:48 +0500

    thirteenth commit

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -2,21 +2,26 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-// import { useState } from "react";
+import { useState } from "react";
 
 function Store() {
 
-  // const [markerPosition, setMarkerPosition] = useState(null);
+  const [markerPosition, setMarkerPosition] = useState([0,0]);
 
-  // const handleMarkerButtonClick = () => {
-  //   // Set the marker position to a specific latitude and longitude
-  //   setMarkerPosition([51.51, -0.12]);
-  // };
+  const handleMarkerButtonClick = () => {
+    // Set the marker position to a specific latitude and longitude
+    setMarkerPosition([24.9103194, 67.0583741]);
+  };
 
   return (
     <>
       <Header />
 
+      <div className="text-center mt-5">
+        <h1>
+          Store Locator
+        </h1>
+      </div>
       <section className="stores">
         <div className="container">
           <div className="row">
@@ -26,7 +31,49 @@ function Store() {
                   <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, consequatur? 
                   </p>
-                  <button className="btn-map">View on map</button>
+                  <button className="btn-map" onClick={handleMarkerButtonClick}>View on map</button>
+                </div>
+                <div className="address">
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, consequatur? 
+                  </p>
+                  <button className="btn-map" onClick={handleMarkerButtonClick}>View on map</button>
+                </div>
+                <div className="address">
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, consequatur? 
+                  </p>
+                  <button className="btn-map" onClick={handleMarkerButtonClick}>View on map</button>
+                </div>
+                <div className="address">
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, consequatur? 
+                  </p>
+                  <button className="btn-map" onClick={handleMarkerButtonClick}>View on map</button>
+                </div>
+                <div className="address">
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, consequatur? 
+                  </p>
+                  <button className="btn-map" onClick={handleMarkerButtonClick}>View on map</button>
+                </div>
+                <div className="address">
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, consequatur? 
+                  </p>
+                  <button className="btn-map" onClick={handleMarkerButtonClick}>View on map</button>
+                </div>
+                <div className="address">
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, consequatur? 
+                  </p>
+                  <button className="btn-map" onClick={handleMarkerButtonClick}>View on map</button>
+                </div>
+                <div className="address">
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, consequatur? 
+                  </p>
+                  <button className="btn-map" onClick={handleMarkerButtonClick}>View on map</button>
                 </div>
               </div>
             </div>
@@ -40,12 +87,11 @@ function Store() {
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 />
-                <Marker position={[51.505, -0.09]}>
+                <Marker position={markerPosition}>
                   <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
                   </Popup>
                 </Marker>
-                {/* <button onClick={handleMarkerButtonClick}>Set Marker</button> */}
               </MapContainer>
             </div>
           </div>
