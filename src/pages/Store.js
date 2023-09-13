f7990a054c3ba3709e8c10751a20e6b68196947d
sajid-ahmed2816 <sajid.ahmed2816@gmail.com>
2023-09-14 04:26:06 +0500

    store page completed

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -16,11 +16,61 @@ function Store() {
   const [markerPosition, setMarkerPosition] = useState(initialMarkerPosition);
   const [showMarker, setShowMarker] = useState(false);
 
-  const handleMarkerButtonClick = () => {
-    // Set the marker position to the specific latitude and longitude
+  const storeLocObj = [
+    {
+      address: 'Shop # 1, Abc Road, XYZ Area, Karachi',
+      phoneNumber: 123456789,
+      location: {
+        latitude: 24.87738663284456,
+        longitude: 67.06261553860841
+      }
+    },
+    {
+      address: 'Shop # 2, Abc Road, XYZ Area, Karachi',
+      phoneNumber: 123456789,
+      location: {
+        latitude: 24.89800435971366,
+        longitude: 67.07728226700415
+      }
+    },
+    {
+      address: 'Shop # 3, Abc Road, XYZ Area, Karachi',
+      phoneNumber: 123456789,
+      location: {
+        latitude: 24.921103533751104, 
+        longitude: 67.03231168213736
+      }
+    },
+    {
+      address: 'Shop # 4, Abc Road, XYZ Area, Karachi',
+      phoneNumber: 123456789,
+      location: {
+        latitude: 24.88940162210062, 
+        longitude: 67.15203807909147
+      }
+    },
+    {
+      address: 'Shop # 5, Abc Road, XYZ Area, Karachi',
+      phoneNumber: 123456789,
+      location: {
+        latitude: 24.804266354404916, 
+        longitude: 67.02909641048079
+      }
+    },
+    {
+      address: 'Shop # 6, Abc Road, XYZ Area, Karachi',
+      phoneNumber: 123456789,
+      location: {
+        latitude: 24.855799445300946, 
+        longitude: 67.01034840432558
+      }
+    }
+  ]
+
+  const handleMarkerButtonClick = (lat, lng) => {
     setMarkerPosition({
-      lat: 24.9103194,
-      lng: 67.0583741
+      lat: lat,
+      lng: lng
     });
     setShowMarker(true);
   };
@@ -39,23 +89,27 @@ function Store() {
           <div className="row">
             <div className="col-md-6">
               <div className="box">
-                <div className="address">
-                  <p>
-                    <span className="icon">
-                      <FontAwesomeIcon icon={faLocationDot}/>  
-                    </span>
-                    <span>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    </span> 
-                  </p>
-                  <p>
-                    <span className="icon">
-                      <FontAwesomeIcon icon={faPhone}/>  
-                    </span>
-                    +12345678 
-                  </p>
-                  <button className="btn-map" onClick={handleMarkerButtonClick}>View on map</button>
-                </div>
+                {storeLocObj.map((item, index) => (
+                  <div key={index} className="address">
+                    <p className="d-flex align-items-center gap-2">
+                      <span className="icon">
+                        <FontAwesomeIcon icon={faLocationDot}/>
+                      </span>
+                      {item.address}
+                    </p>
+                    <p className="d-flex align-items-center gap-2">
+                      <span className="icon">
+                        <FontAwesomeIcon icon={faPhone}/>
+                      </span>
+                      {item.phoneNumber}
+                    </p>
+                    <button className="btn-map"
+                      onClick={() => handleMarkerButtonClick(item.location.latitude, item.location.longitude)}
+                    >
+                      Show on map
+                    </button>
+                  </div>
+                ))}
               </div>
             </div>
             <div className="col-md-6">
