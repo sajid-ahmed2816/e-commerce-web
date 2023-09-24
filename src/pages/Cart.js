c1baf0d4fa7af06c92deaaf1cd4ef258e42ee3f6
sajid-ahmed2816 <sajid.ahmed2816@gmail.com>
2023-09-24 12:22:59 +0500

    update cart page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,14 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Form,
+  InputGroup,
+  ProgressBar,
+} from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import { remove } from "../config/redux/reducer/cartSlice";
 import { useState, useEffect } from "react";
@@ -69,6 +78,33 @@ function Cart() {
   const grandTotal =
     getTotalPrice() === 0 ? 0 : getTotalPrice() + deliveryCharges;
 
+  const [currentStep, setCurrentStep] = useState(1);
+  const [validated, setValidated] = useState(false);
+  const totalSteps = 3;
+  const progress = ((currentStep - 1) / (totalSteps - 1)) * 100;
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  const nextStep = () => {
+    if (currentStep < totalSteps) {
+      setCurrentStep(currentStep + 1);
+    }
+  };
+
+  const prevStep = () => {
+    if (currentStep > 1) {
+      setCurrentStep(currentStep - 1);
+    }
+  };
+
   return (
     <>
       <Header />
@@ -86,7 +122,159 @@ function Cart() {
           <div className="container">
             <div className="row">
               {/* Information Form */}
-              <div className="col-md-7"></div>
+              <div className="col-md-7">
+                <div className="p-4">
+                  <Container>
+                    <Row>
+                      <ProgressBar
+                        now={progress}
+                        label={`${currentStep}/${totalSteps}`}
+                      />
+                    </Row>
+                    <Row>
+                      <Col>
+                        {currentStep === 1 && (
+                          <div>
+                            <Form
+                              noValidate
+                              validated={validated}
+                              onSubmit={handleSubmit}
+                            >
+                              <Row className="mb-3">
+                                <Form.Group
+                                  as={Col}
+                                  md="6"
+                                  controlId="validationCustom01"
+                                >
+                                  <Form.Label>First name</Form.Label>
+                                  <Form.Control
+                                    required
+                                    type="text"
+                                    placeholder="First name"
+                                    defaultValue="Mark"
+                                  />
+                                  <Form.Control.Feedback>
+                                    Looks good!
+                                  </Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group
+                                  as={Col}
+                                  md="6"
+                                  controlId="validationCustom02"
+                                >
+                                  <Form.Label>Last name</Form.Label>
+                                  <Form.Control
+                                    required
+                                    type="text"
+                                    placeholder="Last name"
+                                    defaultValue="Otto"
+                                  />
+                                  <Form.Control.Feedback>
+                                    Looks good!
+                                  </Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group
+                                  as={Col}
+                                  md="12"
+                                  controlId="validationCustomUsername"
+                                >
+                                  <Form.Label>Email</Form.Label>
+                                  <InputGroup hasValidation>
+                                    <InputGroup.Text id="inputGroupPrepend">
+                                      @
+                                    </InputGroup.Text>
+                                    <Form.Control
+                                      type="text"
+                                      placeholder="Email"
+                                      aria-describedby="inputGroupPrepend"
+                                      required
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                      Please enter you email.
+                                    </Form.Control.Feedback>
+                                  </InputGroup>
+                                </Form.Group>
+                              </Row>
+                              <Row className="mb-3">
+                                <Form.Group
+                                  as={Col}
+                                  md="6"
+                                  controlId="validationCustom03"
+                                >
+                                  <Form.Label>City</Form.Label>
+                                  <Form.Control
+                                    type="text"
+                                    placeholder="City"
+                                    required
+                                  />
+                                  <Form.Control.Feedback type="invalid">
+                                    Please provide a valid city.
+                                  </Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group
+                                  as={Col}
+                                  md="3"
+                                  controlId="validationCustom04"
+                                >
+                                  <Form.Label>State</Form.Label>
+                                  <Form.Control
+                                    type="text"
+                                    placeholder="State"
+                                    required
+                                  />
+                                  <Form.Control.Feedback type="invalid">
+                                    Please provide a valid state.
+                                  </Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group
+                                  as={Col}
+                                  md="3"
+                                  controlId="validationCustom05"
+                                >
+                                  <Form.Label>Zip</Form.Label>
+                                  <Form.Control
+                                    type="text"
+                                    placeholder="Zip"
+                                    required
+                                  />
+                                  <Form.Control.Feedback type="invalid">
+                                    Please provide a valid zip.
+                                  </Form.Control.Feedback>
+                                </Form.Group>
+                              </Row>
+                              <Form.Group className="mb-3">
+                                <Form.Check
+                                  required
+                                  label="Agree to terms and conditions"
+                                  feedback="You must agree before submitting."
+                                  feedbackType="invalid"
+                                />
+                              </Form.Group>
+                              <Button type="submit">Submit form</Button>
+                            </Form>
+                          </div>
+                        )}
+                        {currentStep === 2 && <div>Step 2 Content</div>}
+                        {currentStep === 3 && <div>Step 3 Content</div>}
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col className="text-center">
+                        {currentStep > 1 && (
+                          <Button variant="primary" onClick={prevStep}>
+                            Previous
+                          </Button>
+                        )}
+                        {currentStep < 3 && (
+                          <Button variant="primary" onClick={nextStep}>
+                            Next
+                          </Button>
+                        )}
+                      </Col>
+                    </Row>
+                  </Container>
+                </div>
+              </div>
 
               {/* Cart Item Information */}
               <div className="col-md-5 p-5">
